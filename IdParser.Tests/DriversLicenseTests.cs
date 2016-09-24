04dae14f3e98f5934b1b73ac4ed826c7a8cd2ac6
Connor O'Shea <connor@connoroshea.com>
2016-09-24 17:26:12 -0400

    Added Georgia unit test

--- IdParser.Tests/DriversLicenseTests.cs
+++ IdParser.Tests/DriversLicenseTests.cs
@@ -20,6 +20,7 @@ namespace IdParser.Tests {
 
             Assert.AreEqual("Michael", license.LastName);
             Assert.AreEqual(new DateTime(2013, 08, 31), license.DateOfBirth);
+            Assert.AreEqual("New York", license.IssuerIdentificationNumber.GetDescription());
         }
 
         [TestMethod]
@@ -33,6 +34,22 @@ namespace IdParser.Tests {
                 var license = (DriversLicense)idCard;
 
                 Assert.AreEqual("158X9", license.Jurisdiction.RestrictionCodes);
+            }
+        }
+
+        [TestMethod]
+        public void TestGALicense()
+        {
+            var file = File.ReadAllText("GA License.txt");
+            var idCard = IdParser.Parse(file);
+
+            Assert.AreEqual("123 NORTH STATE ST.", idCard.StreetLine1);
+            Assert.AreEqual("Georgia", idCard.IssuerIdentificationNumber.GetDescription());
+
+            if (idCard is DriversLicense) {
+                var license = (DriversLicense)idCard;
+
+                Assert.AreEqual("C", license.Jurisdiction.VehicleClass);
             }
         }
     }
