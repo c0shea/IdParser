37ddd7e0034b5a77fbbdef76a876b63310753339
Connor O'Shea <connor@connoroshea.com>
2016-10-15 18:46:15 -0400

    Added more tests

--- IdParser.Tests/DriversLicenseTests.cs
+++ IdParser.Tests/DriversLicenseTests.cs
@@ -11,8 +11,13 @@ namespace IdParser.Tests {
             var file = File.ReadAllText("MA License 2009.txt");
             var idCard = IdParser.Parse(file, true);
 
+            Assert.AreEqual("ROBERT", idCard.FirstName);
+            Assert.AreEqual("LOWNEY", idCard.MiddleName);
             Assert.AreEqual("SMITH", idCard.LastName);
             Assert.AreEqual("MA", idCard.IssuerIdentificationNumber.GetAbbreviation());
+            Assert.AreEqual(new DateTime(2016, 06, 29), idCard.IssueDate);
+            Assert.AreEqual(new DateTime(1977, 07, 07), idCard.DateOfBirth);
+            Assert.AreEqual(Country.USA, idCard.Country);
         }
 
         [TestMethod]
@@ -20,12 +25,19 @@ namespace IdParser.Tests {
             var file = File.ReadAllText("MA License 2016.txt");
             var idCard = IdParser.Parse(file, true);
 
+            Assert.AreEqual("MORRIS", idCard.FirstName);
+            Assert.AreEqual("T", idCard.MiddleName);
             Assert.AreEqual("SAMPLE", idCard.LastName);
+            Assert.AreEqual(new DateTime(1971, 12, 31), idCard.DateOfBirth);
+
+            Assert.AreEqual("MA", idCard.IssuerIdentificationNumber.GetAbbreviation());
             Assert.AreEqual("S12345678", idCard.IdNumber);
+            Assert.AreEqual(new DateTime(2016, 08, 09), idCard.IssueDate);
+            Assert.AreEqual(Country.USA, idCard.Country);
+
             Assert.AreEqual("24 BEACON STREET", idCard.StreetLine1);
             Assert.AreEqual("MA504", idCard.AdditionalJurisdictionElements.Single(e => e.Key == "ZMZ").Value);
             Assert.AreEqual("02133-0000", idCard.FormattedPostalCode);
-            Assert.AreEqual("MA", idCard.IssuerIdentificationNumber.GetAbbreviation());
 
             if (idCard is DriversLicense) {
                 var license = (DriversLicense)idCard;
@@ -40,10 +52,19 @@ namespace IdParser.Tests {
             var file = File.ReadAllText("NY License.txt");
             var license = IdParser.Parse(file);
 
+            Assert.AreEqual("M", license.FirstName);
+            Assert.AreEqual("Motorist", license.MiddleName);
             Assert.AreEqual("Michael", license.LastName);
             Assert.AreEqual(new DateTime(2013, 08, 31), license.DateOfBirth);
+
             Assert.AreEqual("New York", license.IssuerIdentificationNumber.GetDescription());
             Assert.AreEqual("NY", license.IssuerIdentificationNumber.GetAbbreviation());
+            Assert.AreEqual(new DateTime(2013, 08, 31), license.IssueDate);
+            Assert.AreEqual(new DateTime(2013, 08, 31), license.ExpirationDate);
+            Assert.AreEqual(Country.USA, license.Country);
+
+            Assert.AreEqual("2345 ANYWHERE STREET", license.StreetLine1);
+            Assert.AreEqual("YOUR CITY", license.City);
         }
 
         [TestMethod]
@@ -51,8 +72,19 @@ namespace IdParser.Tests {
             var file = File.ReadAllText("VA License.txt");
             var idCard = IdParser.Parse(file);
 
-            Assert.AreEqual("STAUNTON", idCard.City);
+            Assert.AreEqual("JUSTIN", idCard.FirstName);
+            Assert.AreEqual("WILLIAM", idCard.MiddleName);
+            Assert.AreEqual("MAURY", idCard.LastName);
+            Assert.AreEqual(new DateTime(1958, 07, 15), idCard.DateOfBirth);
+
             Assert.AreEqual("VA", idCard.IssuerIdentificationNumber.GetAbbreviation());
+            Assert.AreEqual(new DateTime(2009, 08, 14), idCard.IssueDate);
+            Assert.AreEqual(new DateTime(2017, 08, 14), idCard.ExpirationDate);
+            Assert.AreEqual(Country.USA, idCard.Country);
+
+            Assert.AreEqual("17 FIRST STREET", idCard.StreetLine1);
+            Assert.AreEqual("STAUNTON", idCard.City);
+            Assert.AreEqual("244010000", idCard.PostalCode);
 
             if (idCard is DriversLicense) {
                 var license = (DriversLicense)idCard;
@@ -62,42 +94,73 @@ namespace IdParser.Tests {
         }
 
         [TestMethod]
-        public void TestGALicense()
-        {
+        public void TestGALicense() {
             var file = File.ReadAllText("GA License.txt");
             var idCard = IdParser.Parse(file);
 
+            Assert.AreEqual("JANICE", idCard.FirstName);
+            Assert.IsNull(idCard.MiddleName);
+            Assert.AreEqual("SAMPLE", idCard.LastName);
+            Assert.AreEqual("PH.D.", idCard.NameSuffix);
+            Assert.AreEqual(new DateTime(1957, 07, 01), idCard.DateOfBirth);
+
             Assert.AreEqual("123 NORTH STATE ST.", idCard.StreetLine1);
+            Assert.AreEqual("ANYTOWN", idCard.City);
+            Assert.AreEqual("303340000", idCard.PostalCode);
+
             Assert.AreEqual("Georgia", idCard.IssuerIdentificationNumber.GetDescription());
             Assert.AreEqual("GA", idCard.IssuerIdentificationNumber.GetAbbreviation());
+            Assert.AreEqual(new DateTime(2006, 07, 01), idCard.IssueDate);
+            Assert.AreEqual(Country.USA, idCard.Country);
 
             if (idCard is DriversLicense) {
                 var license = (DriversLicense)idCard;
 
+                Assert.AreEqual("NONE", license.Jurisdiction.RestrictionCodes);
                 Assert.AreEqual("C", license.Jurisdiction.VehicleClass);
+                Assert.AreEqual("P", license.Jurisdiction.EndorsementCodes);
             }
         }
 
         [TestMethod]
-        public void TestCTLicense()
-        {
+        public void TestCTLicense() {
             var file = File.ReadAllText("CT License.txt");
             var idCard = IdParser.Parse(file, true);
 
-            Assert.IsTrue(idCard.IsOrganDonor);
+            Assert.AreEqual("ADULT", idCard.FirstName);
+            Assert.AreEqual("A", idCard.MiddleName);
             Assert.AreEqual("CTLIC", idCard.LastName);
-            Assert.AreEqual("990000001", idCard.IdNumber);
+            Assert.AreEqual(new DateTime(1961, 01, 01), idCard.DateOfBirth);
+
+            Assert.AreEqual(new Height(Version.Aamva2013, "066 in"), idCard.Height);
+            Assert.AreEqual("BLU", idCard.EyeColor);
+            Assert.IsTrue(idCard.IsOrganDonor);
+
+            Assert.AreEqual("60 STATE ST", idCard.StreetLine1);
+            Assert.AreEqual("WETHERSFIELD", idCard.City);
+            Assert.AreEqual("061091896", idCard.PostalCode);
+
             Assert.AreEqual("CT", idCard.IssuerIdentificationNumber.GetAbbreviation());
+            Assert.AreEqual("990000001", idCard.IdNumber);
+            Assert.AreEqual(new DateTime(2015, 01, 01), idCard.ExpirationDate);
+            Assert.AreEqual(Country.USA, idCard.Country);
+
+            if (idCard is DriversLicense) {
+                var license = (DriversLicense)idCard;
+
+                Assert.AreEqual("D", license.Jurisdiction.VehicleClass);
+                Assert.AreEqual("B", license.Jurisdiction.RestrictionCodes);
+            }
         }
 
         [TestMethod]
-        public void TestNonStandardDataElementSeparator()
-        {
+        public void TestNonStandardDataElementSeparator() {
             var file = File.ReadAllText("MA License Piped.txt");
             var idCard = IdParser.Parse(file, true);
 
             Assert.AreEqual("S65807412", idCard.IdNumber);
             Assert.AreEqual("123 MAIN STREET", idCard.StreetLine1);
+            Assert.AreEqual(Country.USA, idCard.Country);
             Assert.IsInstanceOfType(idCard, typeof(DriversLicense));
         }
     }
