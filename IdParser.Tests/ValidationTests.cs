619467987c16f9891b24005a0d0295e12e48a899
Connor O'Shea <connor@connoroshea.com>
2017-08-10 21:17:10 -0400

    Add ValidationTests

---
+++ IdParser.Tests/ValidationTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IdParser.Tests
+{
+    [TestClass]
+    public class ValidationTests
+    {
+        [TestMethod]
+        public void InvalidLengthTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => IdParser.Parse("ABC123"));
+        }
+
+        [TestMethod]
+        public void InvalidComplianceIndicatorTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => IdParser.Parse(new string('A', 32)));
+        }
+    }
+}
