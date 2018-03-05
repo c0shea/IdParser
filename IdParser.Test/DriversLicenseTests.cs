1514f96fa74d870926b0acf93c83606b47dbb192
Connor O'Shea <connor@connoroshea.com>
2018-03-04 20:18:29 -0500

    Add test for Tennessee ID

--- IdParser.Test/DriversLicenseTests.cs
+++ IdParser.Test/DriversLicenseTests.cs
@@ -7,7 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace IdParser.Test
 {
     [TestClass]
-    public class DriversLicenseTests
+    public class DriversLicenseTests : BaseTest
     {
         [TestMethod]
         public void TestMA2009License()
@@ -1479,77 +1479,6 @@ namespace IdParser.Test
 
             Assert.AreEqual("87544", idCard.Address.PostalCodeDisplay);
             Assert.AreEqual("New Mexico", idCard.IssuerIdentificationNumber.GetDescription());
-        }
-
-        private void AssertIdCard(IdentificationCard expected, IdentificationCard actual)
-        {
-            Assert.IsNotNull(expected);
-            Assert.IsNotNull(actual);
-
-            Assert.AreEqual(expected.Name.First, actual.Name.First, nameof(actual.Name.First));
-            Assert.AreEqual(expected.Name.Middle, actual.Name.Middle, nameof(actual.Name.Middle));
-            Assert.AreEqual(expected.Name.Last, actual.Name.Last, nameof(actual.Name.Last));
-            Assert.AreEqual(expected.Name.Suffix, actual.Name.Suffix, nameof(actual.Name.Suffix));
-
-            Assert.AreEqual(expected.Name.WasFirstTruncated, actual.Name.WasFirstTruncated, nameof(actual.Name.WasFirstTruncated));
-            Assert.AreEqual(expected.Name.WasMiddleTruncated, actual.Name.WasMiddleTruncated, nameof(actual.Name.WasMiddleTruncated));
-            Assert.AreEqual(expected.Name.WasLastTruncated, actual.Name.WasLastTruncated, nameof(actual.Name.WasLastTruncated));
-
-            Assert.AreEqual(expected.Address.City, actual.Address.City, nameof(actual.Address.City));
-            Assert.AreEqual(expected.Address.StreetLine1, actual.Address.StreetLine1, nameof(actual.Address.StreetLine1));
-            Assert.AreEqual(expected.Address.StreetLine2, actual.Address.StreetLine2, nameof(actual.Address.StreetLine2));
-            Assert.AreEqual(expected.Address.JurisdictionCode, actual.Address.JurisdictionCode, nameof(actual.Address.JurisdictionCode));
-            Assert.AreEqual(expected.Address.JurisdictionCode, actual.IssuerIdentificationNumber.GetAbbreviation(), nameof(actual.IssuerIdentificationNumber));
-            Assert.AreEqual(expected.Address.PostalCode, actual.Address.PostalCode, nameof(actual.Address.PostalCode));
-            Assert.AreEqual(expected.Address.Country, actual.Address.Country, nameof(actual.Address.Country));
-
-            Assert.AreEqual(expected.DateOfBirth, actual.DateOfBirth, nameof(actual.DateOfBirth));
-            Assert.AreEqual(expected.PlaceOfBirth, actual.PlaceOfBirth, nameof(actual.PlaceOfBirth));
-            Assert.AreEqual(expected.Sex, actual.Sex, nameof(actual.Sex));
-            Assert.AreEqual(expected.Height, actual.Height, nameof(actual.Height));
-            Assert.AreEqual(expected.Weight, actual.Weight, nameof(actual.Weight));
-
-            Assert.AreEqual(expected.EyeColor, actual.EyeColor, nameof(actual.EyeColor));
-            Assert.AreEqual(expected.HairColor, actual.HairColor, nameof(actual.HairColor));
-            Assert.AreEqual(expected.Ethnicity, actual.Ethnicity, nameof(actual.Ethnicity));
-
-            Assert.AreEqual(expected.IdNumber, actual.IdNumber, nameof(actual.IdNumber));
-            Assert.AreEqual(expected.AamvaVersionNumber, actual.AamvaVersionNumber, nameof(actual.AamvaVersionNumber));
-
-            Assert.AreEqual(expected.IssueDate, actual.IssueDate, nameof(actual.IssueDate));
-            Assert.AreEqual(expected.ExpirationDate, actual.ExpirationDate, nameof(actual.ExpirationDate));
-            Assert.AreEqual(expected.RevisionDate, actual.RevisionDate, nameof(actual.RevisionDate));
-
-            Assert.AreEqual(expected.Under18Until, actual.Under18Until, nameof(actual.Under18Until));
-            Assert.AreEqual(expected.Under19Until, actual.Under19Until, nameof(actual.Under19Until));
-            Assert.AreEqual(expected.Under21Until, actual.Under21Until, nameof(actual.Under21Until));
-
-            Assert.AreEqual(expected.ComplianceType, actual.ComplianceType, nameof(actual.ComplianceType));
-            Assert.AreEqual(expected.HasTemporaryLawfulStatus, actual.HasTemporaryLawfulStatus, nameof(actual.HasTemporaryLawfulStatus));
-            Assert.AreEqual(expected.IsOrganDonor, actual.IsOrganDonor, nameof(actual.IsOrganDonor));
-            Assert.AreEqual(expected.IsVeteran, actual.IsVeteran, nameof(actual.IsVeteran));
-        }
-
-        private void AssertLicense(DriversLicense expected, IdentificationCard actualId)
-        {
-            Assert.IsNotNull(expected);
-            Assert.IsNotNull(actualId);
-            Assert.IsInstanceOfType(actualId, typeof(DriversLicense));
-
-            var actual = (DriversLicense)actualId;
-            Assert.IsNotNull(actual.Jurisdiction);
-
-            Assert.AreEqual(expected.Jurisdiction.VehicleClass, actual.Jurisdiction.VehicleClass, nameof(actual.Jurisdiction.VehicleClass));
-            Assert.AreEqual(expected.Jurisdiction.RestrictionCodes, actual.Jurisdiction.RestrictionCodes, nameof(actual.Jurisdiction.RestrictionCodes));
-            Assert.AreEqual(expected.Jurisdiction.EndorsementCodes, actual.Jurisdiction.EndorsementCodes, nameof(actual.Jurisdiction.EndorsementCodes));
-            Assert.AreEqual(expected.Jurisdiction.VehicleClassificationDescription, actual.Jurisdiction.VehicleClassificationDescription, nameof(actual.Jurisdiction.VehicleClassificationDescription));
-            Assert.AreEqual(expected.Jurisdiction.EndorsementCodeDescription, actual.Jurisdiction.EndorsementCodeDescription, nameof(actual.Jurisdiction.EndorsementCodeDescription));
-            Assert.AreEqual(expected.Jurisdiction.RestrictionCodeDescription, actual.Jurisdiction.RestrictionCodeDescription, nameof(actual.Jurisdiction.RestrictionCodeDescription));
-
-            Assert.AreEqual(expected.StandardVehicleClassification, actual.StandardVehicleClassification, nameof(actual.StandardVehicleClassification));
-            Assert.AreEqual(expected.StandardEndorsementCode, actual.StandardEndorsementCode, nameof(actual.StandardEndorsementCode));
-            Assert.AreEqual(expected.StandardRestrictionCode, actual.StandardRestrictionCode, nameof(actual.StandardRestrictionCode));
-            Assert.AreEqual(expected.HazmatEndorsementExpirationDate, actual.HazmatEndorsementExpirationDate, nameof(actual.HazmatEndorsementExpirationDate));
-        }
+        }        
     }
 }
---
+++ IdParser.Test/IdentificationCardTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable InconsistentNaming
+
+namespace IdParser.Test
+{
+    [TestClass]
+    public class IdentificationCardTests : BaseTest
+    {
+        [TestMethod]
+        public void TestTNIdCard()
+        {
+            var expected = new IdentificationCard
+            {
+                Name = new Name
+                {
+                    First = "ELIZABETH",
+                    Middle = "MOTORIST",
+                    Last = "SMITH",
+
+                    WasFirstTruncated = false,
+                    WasMiddleTruncated = false,
+                    WasLastTruncated = false
+                },
+
+                Address = new Address
+                {
+                    StreetLine1 = "21078 MAGNOLIA RD",
+                    City = "NASHVILLE",
+                    JurisdictionCode = "TN",
+                    PostalCode = "370115509",
+                    Country = Country.Usa
+                },
+
+                DateOfBirth = new DateTime(1961, 12, 13),
+                Sex = Sex.Female,
+                EyeColor = EyeColor.Green,
+                Height = Height.FromImperial(63),
+
+                IdNumber = "115775955",
+                AamvaVersionNumber = Version.Aamva2011,
+
+                IssueDate = new DateTime(2018, 02, 06),
+                ExpirationDate = new DateTime(2026, 02, 06),
+                RevisionDate = new DateTime(2011, 12, 02),
+
+                IsOrganDonor = true
+            };
+
+            var file = File.ReadAllText("TN ID.txt");
+            var idCard = Barcode.Parse(file, Validation.None);
+
+            AssertIdCard(expected, idCard);
+
+            Assert.AreEqual("37011-5509", idCard.Address.PostalCodeDisplay);
+            Assert.AreEqual("Tennessee", idCard.IssuerIdentificationNumber.GetDescription());
+        }
+    }
+}
