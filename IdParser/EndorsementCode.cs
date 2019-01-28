9c956597c65cce36e4e04a1dd112432d5f76fa4a
Connor O'Shea <connor@connoroshea.com>
2019-01-27 22:45:42 -0500

    Add EndorsementCode enum

---
+++ IdParser/EndorsementCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using IdParser.Attributes;
+
+namespace IdParser
+{
+    [Flags]
+    public enum EndorsementCode
+    {
+        None = 0,
+
+        [Abbreviation("N")]
+        Tank = 1,
+
+        [Abbreviation("P")]
+        Passenger = 2,
+
+        [Abbreviation("S")]
+        [Description("School Bus")]
+        SchoolBus = 4,
+
+        [Abbreviation("T")]
+        [Description("Doubles/Triples")]
+        DoublesTriples = 8,
+
+        [Abbreviation("H")]
+        [Description("Hazardous Material")]
+        HazardousMaterial = 16,
+
+        [Abbreviation("X")]
+        [Description("Combined Tank/Hazardous Material")]
+        CombinedTankHazardousMaterial = Tank | HazardousMaterial,
+
+        [Abbreviation("L")]
+        Motorcycles = 32,
+
+        [Abbreviation("O")]
+        [Description("Other Jurisdiction Specific Endorsements")]
+        Other = 64
+    }
+}
