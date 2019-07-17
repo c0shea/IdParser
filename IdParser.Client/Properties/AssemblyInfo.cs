4efbdda1664c6ff7f6ce8c127e89747365b1c0ac
Connor O'Shea <connor@connoroshea.com>
2019-07-16 21:29:03 -0400

    Release v4.1.2

--- IdParser.Client/Properties/AssemblyInfo.cs
+++ IdParser.Client/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ID Parser")]
-[assembly: AssemblyCopyright("Copyright © 2018 Connor O'Shea")]
+[assembly: AssemblyCopyright("Copyright © 2019 Connor O'Shea")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.1.2")]
+[assembly: AssemblyFileVersion("4.1.2")]
